b55073e2ef4f350d3a1eda4181f10985943ff5c3
thaseda saidani <thasedasaidani2000@gmail.com>
2025-08-09 00:18:26 +0200

    table

---
+++ demo2.php
@@ -0,0 +1,29 @@
+<?php
+$classe = [
+   [
+    'nom' => 'meroua',
+    'prenom' => 'abdelene',
+    'notes'=> [15 , 16 , 20]
+    ],
+
+    [
+    'nom' => 'miness',
+    'prenom' => 'rabdelene',
+    'notes'=> [5 , 16 , 20]
+    ],
+
+    [
+    'nom' => 'mera',
+    'prenom' => 'babdelene',
+    'notes'=> [0 , 16 , 20]
+    ],
+    [
+    'nom' => 'merinda',
+    'prenom' => 'abdelenes',
+    'notes'=> [7 , 16 , 20]
+],
+
+
+];
+echo $classe[2]['notes'][0] . '  ' . $classe[2]['nom'];
+?>
