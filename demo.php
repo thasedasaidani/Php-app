025b0991ea047f6d156a715d7f7b1392968361b0
thaseda saidani <thasedasaidani2000@gmail.com>
2025-08-08 23:43:38 +0200

    VARIABLE

--- demo.php
+++ demo.php
@@ -1,1 +1,8 @@
-salut les gars 
+<?php
+$nom = 'dina';
+$prenom = 'zidane';
+$note1 = 10;
+$note2  = 20;
+$moyenne = ($note1 + $note2) / 2;
+echo   "bonjour $nom $prenom  vous avez eu $moyenne  de moyenne";
+?>
