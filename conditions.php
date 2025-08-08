790349c68a75e6ee57b2b576a7e4eec65373b650
thaseda saidani <thasedasaidani2000@gmail.com>
2025-08-09 01:55:39 +0200

    conditions

--- conditions.php
+++ conditions.php
@@ -6,9 +6,37 @@ if ($note > 15) {
     echo ' VOUS AVEZ LA MENTION BIEN' . "\n";
 };
 
-$moyenne = readline('ecriver votre moyenne  :');
-if ($moyenne >= 10) {
-    echo ' VOUS AVEZ DE LA MOYENNE ';
+$moyenne = (int)readline('ecriver votre moyenne  :');
+if ($moyenne === 10) {
+    echo 'vous avez juste la moyenne ' . "\n";
+} elseif ($moyenne > 10) {
+    echo ' VOUS AVEZ DE LA MOYENNE ' . "\n";
 } else {
-    echo ' VOUS N AVEZ PAS DE MOYENNE ';
+    echo ' VOUS N AVEZ PAS DE MOYENNE'  . "\n";
+};
+
+
+// switch
+$action  = (int)readline("Écrivez votre action : (1 : j'attaque, 2 : je défends, 3 : je me soigne) : ");
+
+switch ($action) {
+    case 1:
+        echo "j'attaque" . "\n";
+        break;
+    case 2:
+        echo "je défends" . "\n";
+        break;
+    case 3:
+        echo "je me soigne " . "\n";
+        break;
+    default:
+        echo "je ne fais rien " . "\n";
+};
+
+// exemple heure 
+$heure = (int)readline('entrez une heure :');
+if (($heure >= 8 && $heure < 12) || ($heure >= 14 && $heure < 18)) {
+    echo ' le magasin est ouvert ';
+} else {
+    echo ' le magasin est ferme';
 }
